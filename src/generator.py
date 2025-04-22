769b1e1c21b8c5d75314cbf407b027e03c26e673
Tartas37 <aleksejs.makejevs@gmail.com>
2025-04-22 17:03:40 +0300

    Added implicit types

--- src/generator.py
+++ src/generator.py
@@ -1,23 +1,68 @@
-def generate_c(ast):
-    def add_io(code):
-        return f'#include <stdio.h>\n' + code
 
-    c_code = "int main(void) {\n"
+def generate_c(ast):
+    c_code = """#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "uthash.h"
+
+typedef struct {
+    char key[32];
+    char *value;
+    UT_hash_handle hh;
+} HashItem;
+
+int main() {
+    HashItem *table = NULL, *item;
+"""
+
+    def add_variable(c_code, ident, var_type):
+        c_code += f'    item = malloc(sizeof(HashItem));\n'
+        c_code += f'    strcpy(item->key, "{ident}");\n'
+        c_code += f'    item->value = \"{var_type}\";\n'
+        c_code += f'    HASH_ADD_STR(table, key, item);\n'
+        return c_code
+
+    def print_variable(c_code, ident):
+        c_code += f"    HASH_FIND_STR(table, \"{ident}\", item);\n"
+        c_code += '     if (strcmp("int", item->value) == 0) {\n'
+        c_code += f'        printf("%i\\n", {ident});\n'
+        c_code += '     }\n'
+        c_code += '     else if (strcmp("string", item->value) == 0){\n'
+        c_code += f'        printf("%s\\n", {ident});\n'
+        c_code += '     }\n'
+        return c_code
 
     for statement in ast:
         print(statement)
         if statement[0] == "print":
-            add_io(c_code)
-            c_code += f'    printf("{statement[1][1]}\\n");\n'
+            if statement[1][0] == "number":
+                c_code += f'    printf("%i\\n", {statement[1][1]});\n'
+            elif statement[1][0] == "string":
+                c_code += f'    printf("%s\\n", "{statement[1][1]}");\n'
+            elif statement[1][0] == "ident":
+                c_code = print_variable(c_code, statement[1][1])
+            else:
+                raise SyntaxError("Unknown print syntax!")
         elif statement[0] == "return":
             c_code += f'    return {statement[1][1]};\n'
         elif statement[0] == "assign":
             if statement[2][0] == "number":
                 c_code += f'    int {statement[1]} = {statement[2][1]};\n'
+                c_code = add_variable(c_code, statement[1], "int")
+            elif statement[2][0] == "STRING":
+                c_code += f'    char *{statement[1]} = "{statement[2][1]}";\n'
+                c_code = add_variable(c_code, statement[1], "string")
         else:
             raise SyntaxError("Unknown statement syntax!")
 
-    return c_code + "}\n"
+    return c_code + """
+    HashItem *tmp;
+    HASH_ITER(hh, table, item, tmp) {
+        HASH_DEL(table, item);
+        free(item);
+    }
+}
+        """
         #
         # ("PRINT", r"print"),
         # ("RETURN", r"return"),
--- src/parser.py
+++ src/parser.py
@@ -1,14 +1,10 @@
 from re import findall, fullmatch
 
 def tokenize(code):
-
     TOKENS = (
         ("PRINT", r"print"),
         ("RETURN", r"return"),
         ("FUN", r"function"),
-
-        ("NUMBER", r"\d+"),
-        ("IDENT", r"[a-zA-Z_]\w*"),
 
         ("PLUS", r"\+"),
         ("MINUS", r"\-"),
@@ -21,24 +17,28 @@ def tokenize(code):
         ("RPAREN", r"\)"),
 
         ("SEMICOLON", r";"),
+
+        ("STRING", r'"[^"\n]*"'),
+        ("NUMBER", r"\d+"),
+        ("IDENT", r"[a-zA-Z_]\w*"),
     )
 
     def find_token(string):
         for token, regex in TOKENS:
             if fullmatch(regex, string):
                 return token
-
         raise SyntaxError(f"Unknown token: {string}")
 
     output = []
 
-    tokens = findall(r'\w+|==|[-+*/=;()]', code)
+    tokens = findall(r'"[^"\n]*"|\w+|==|[-+*/=;()]', code)
 
     for word in tokens:
         token = find_token(word)
         output.append((token, word))
 
     return output
+
 def parse(tokens):
     pos = 0
 
@@ -72,6 +72,9 @@ def parse(tokens):
         if tok_type == "NUMBER":
             pos += 1
             return ("number", int(value))
+        elif tok_type == "STRING":
+            pos += 1
+            return ("STRING", value[1:-1])
         elif tok_type == "IDENT":
             pos += 1
             return ("ident", value)
@@ -104,14 +107,10 @@ def parse(tokens):
             pos += 1
             expect("LPAREN")
             expect("RPAREN")
-            expect("LBRACE")  # еще не добавили в токены, пока можно убрать или заменить
-            body = []
-            while not match("RBRACE"):
-                body.append(parse_statement())
-            return ("function", name, body)
+            expect("SEMICOLON")
+            return ("function", name, [])
 
         elif current()[0] == "IDENT":
-            # переменная = выражение;
             name = current()[1]
             pos += 1
             expect("EQUAL")
@@ -122,7 +121,6 @@ def parse(tokens):
         else:
             raise SyntaxError(f"Unknown statement: {current()}")
 
-    # основной цикл
     ast = []
     while current()[0] != "EOF":
         ast.append(parse_statement())
